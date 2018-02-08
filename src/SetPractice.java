cd922ea490fdc9f9d2cfc20822b5f190b2781e6a
SupalukBenz <supaluk.j@ku.th>
2018-02-08 15:01:46 +0700

    Commit set-practice

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -4,6 +4,7 @@ import java.util.*;
  * Practice using a Set.
  */
 public class SetPractice {
+
 	/**
 	 * Add zero or more items to the set, and print what is being added.
 	 * @parameter set is the Set to add items to
@@ -12,60 +13,81 @@ public class SetPractice {
 	public static void addAll(Set<String> set, String ...values) {
 		// add all the values to the set. 
 		// print each one as you add it
+		System.out.println("{ ");
+		for(String s : values){
+			set.add(s);
+			System.out.print(s + " ");
+		}
+		System.out.println("}");
 	}
 	
 	/**
 	 * Print all the elements in a set, using an Iterator.
 	 */
-	public static void print(Set<String> set) {
+	public static void print(Collection<String> set) {
 		// create an Iterator using set.iterator()
 		// then use the iterator to print everything in the set
+		Iterator<String> iterator = set.iterator();
+		System.out.print("( ");
+		while(iterator.hasNext()){
+			System.out.print(iterator.next() + " ");
+		}
+		System.out.println(" )");
 	}
 	
 	/**
 	 * Explore behavior of a Set.
 	 */
 	public static void setExplorer( ) {
-		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
 		
-		
-		// 2. Print elements in the set using an Iterator over the Set.
-		// You're going to need to do this again, so write a method print(Set set) to do it.
-		
-		
-		// 3. are the elements printed in same order than you added them? 
-		// What does this tell you about Sets?
-		
-		
-		// 4. Remove all the elements from the set, using a method that 
-		// defined in Set (and Collection). What is the method to use?
-		
-		
-		// 5. Add same elements as exercise 1, but in a different order,
-		// such as: "cat", "elephant", "bird", "ant", "dog"
-		
-		
-		// 6. Print the elements in the set.  Is the order same as before?
-		
-		
-		// 7. Add duplicate elements to the set.  Add another "cat" and "dog".
-		// To ensure that these are *distinct* objects use 'set.add(new String("cat"))'
-		// and new String("dog").
-		System.out.println("Add duplicate items to the set ");
-		
-		
-		// 8. Print the elements in set.  Are there any duplicates?
-		// What property of Set does this demonstrate?
-		System.out.println("Set contains: ");
-		
-		
-		// 9. Repeat the exercises using a TreeSet instead of HashSet.
-		//  What is different when you use TreeSet?
-		
-		// Hint: Use Polymorphism.  Make the Set object be a *parameter* to the
-		// method so you can easily use a different set just by changing the parameter!
-		
-		// 10. Repeat again using TreeSet with a Comparator.
+		HashSet<String> hashSet = new HashSet<String>();
+		System.out.println("Using HashSet");
+		setCollection(hashSet);
+
+		TreeSet<String> treeSet = new TreeSet<String>();
+		System.out.println("Using TreeSet");
+		setCollection(treeSet);
+
+		System.out.println("Using TreeSet with a Comparator");
+		Comparator<String> comparator = new StringComparator();
+		TreeSet<String> treeSet1 = new TreeSet<String>();
+		setCollection(treeSet1);
+
+	}
+
+	public static void setCollection(Collection<String> collection){
+		System.out.println("----------------------");
+
+		collection.add("ant");
+		collection.add("bird");
+		collection.add("cat");
+		collection.add("dog");
+		collection.add("elephant");
+
+		System.out.println("Add : ant , bird , cat , dog , elephant");
+		System.out.print("Set contains: ");
+		print(collection);
+		System.out.print("Remove : ");
+		print(collection);
+		collection.clear();
+
+		collection.add("cat");
+		collection.add("elephant");
+		collection.add("bird");
+		collection.add("ant");
+		collection.add("dog");
+		System.out.println("Add : cat , elephant , bird , ant , dog");
+		System.out.print("Set contains: ");
+		print(collection);
+
+		System.out.println("Add duplicate items to the set : cat , dog");
+		collection.add("cat");
+		collection.add("dog");
+
+		System.out.print("Set contains: ");
+		print(collection);
+		System.out.println("----------------------");
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
